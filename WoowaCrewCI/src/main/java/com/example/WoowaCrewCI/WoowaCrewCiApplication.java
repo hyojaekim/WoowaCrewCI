ec40a0b5568c79269bd7d42a837e872e93df4286
yk1028 <wongeunsong94@gmail.com>
2019-12-10 15:48:16 +0900

    init

---
+++ WoowaCrewCI/src/main/java/com/example/WoowaCrewCI/WoowaCrewCiApplication.java
@@ -0,0 +1,13 @@
+package com.example.WoowaCrewCI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoowaCrewCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoowaCrewCiApplication.class, args);
+	}
+
+}
---
+++ WoowaCrewCI/src/test/java/com/example/WoowaCrewCI/WoowaCrewCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WoowaCrewCI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoowaCrewCiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
