4e08a894d05f6c21c3edc2cbdc7d92c8492dc423
pkch93 <star_final@naver.com>
2019-12-11 15:26:29 +0900

    feat: index.html

--- WoowaCrewCI/src/main/java/com/example/WoowaCrewCI/WoowaCrewCiApplication.java
+++ WoowaCrewCI/src/main/java/com/example/WoowaCrewCI/WoowaCrewCiApplication.java
@@ -2,12 +2,15 @@ package com.example.WoowaCrewCI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class WoowaCrewCiApplication {
+public class WoowaCrewCiApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WoowaCrewCiApplication.class, args);
 	}
-
 }
